aeb1fd6ccb1e6a7c773ed0850201ae04dfbff1d9
mb0850 <mayukhbasu5190@gmail.com>
2026-06-13 21:46:49 +0530

    [1] setup & loop

--- src/main.rs
+++ src/main.rs
@@ -34,4 +34,44 @@ impl Game {
 }
 
 #[macroquad::main("Crablo")]
-async fn main() {}
+async fn main() {
+    let mut game = Game::new();
+    let mut state = AppState::Menu;
+
+    loop {
+        clear_background(WHITE);
+
+        match state {
+            AppState::Menu => {
+                draw_text("Menu - Enter to start", 100., 100., 40., BLACK);
+                if is_key_pressed(KeyCode::Enter) {
+                    game = Game::new();
+                    state = AppState::Playing;
+                }
+            }
+            AppState::Playing => {
+                if game.update(get_frame_time()) {
+                    state = AppState::GameOver;
+                }
+                game.draw();
+            }
+            AppState::GameOver => {
+                game.draw();
+                draw_rectangle(
+                    0.,
+                    0.,
+                    screen_width(),
+                    screen_height(),
+                    Color::new(1., 1., 1., 0.7),
+                );
+                draw_text("Game Over", 100., 100., 60., RED);
+                draw_text("Enter to reset", 100., 150., 20., GRAY);
+
+                if is_key_pressed(KeyCode::Enter) {
+                    state = AppState::Menu;
+                }
+            }
+        }
+        next_frame().await;
+    }
+}
